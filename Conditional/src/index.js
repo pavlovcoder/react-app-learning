f4aec4fa140ca85cdfd26383b25acbe4388d8969
Vladimir Pavlov <thepavlovcompany@gmail.com>
2018-01-23 21:59:42 +0100

    Completed app
    Completed with new basic functionalities base on React

--- Conditional/src/index.js
+++ Conditional/src/index.js
@@ -396,4 +396,66 @@ ReactDOM.render(
     <FileInput />,
     document.getElementById("react-section-6")
 );
+
+//Multiple Inputs Maintenance:
+class Reservation extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            isGoing: true,
+            numberOfGuests: 2
+        };
+        
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+    
+    handleInputChange(event){
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        
+        this.setState({
+            [name]: value
+        });
+    }
+    
+    render(){
+        return(
+            <form>
+                <label>
+                    Is going:
+                    <input 
+                        name="isGoing"
+                        type="checkbox"
+                        checked={this.state.isGoing}
+                        onChange={this.handleInputChange}
+                    />
+                </label>
+                <br/>
+                <label>
+                    Number of guests:
+                    <input 
+                        name="numberOfGuest"
+                        type="number"
+                        value={this.state.numberOfGuest}
+                        onChange={this.handleInputChange}
+                    />
+                </label>
+            </form>    
+        );
+    }
+}
+
+ReactDOM.render(
+    <Reservation />,
+    document.getElementById('react-section-7')
+);
+
+//Controlled input null value:
+setTimeout(function(){
+    ReactDOM.render(<input value={null}/>,
+    document.getElementById("react-setion8")
+    );
+},1000);
+
 registerServiceWorker();

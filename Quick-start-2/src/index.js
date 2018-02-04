ed656960d206f380e2ae9f9f1a841a157c58404e
Vladimir Pavlov <thepavlovcompany@gmail.com>
2018-02-04 22:21:50 +0100

    Update index.js

--- Quick-start-2/src/index.js
+++ Quick-start-2/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import PropTypes from 'prop-types';
 
 const scaleNames = {
   c: 'Celsius',
@@ -135,8 +136,404 @@ class Calculator extends React.Component {
   }
 }
 
+//Developing mini app for greetings new persons aboard with React Components:
+function FancyBorder(props) {
+  return (
+    <div className={'FancyBorder FancyBorder-' + props.color}>
+      {props.children}
+    </div>
+  );
+}
+
+function Dialog(props) {
+  return (
+    <FancyBorder color="blue">
+      <h1 className="Dialog-title">
+        {props.title}
+      </h1>
+      <p className="Dialog-message">
+        {props.message}
+      </p>
+      {props.children}
+    </FancyBorder>
+  );
+}
+
+class SignUpDialog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+    this.state = {login: ''};
+  }
+
+  render() {
+    return (
+      <Dialog title="Mars Exploration Program"
+              message="How should we refer to you?">
+        <input value={this.state.login}
+               onChange={this.handleChange} />
+        <button onClick={this.handleSignUp}>
+          Sign Me Up!
+        </button>
+      </Dialog>
+    );
+  }
+
+  handleChange(e) {
+    this.setState({login: e.target.value});
+  }
+
+  handleSignUp() {
+    alert(`Welcome aboard, ${this.state.login}!`);
+  }
+}
+
+//Developing a new table sorter mini application for books e-shop:
+class BookCategoryRow extends React.Component {
+    render() {
+        const category = this.props.category;
+        return (
+            <tr>
+                <th colSpan="2">
+                    {category}
+                </th>
+            </tr>
+        );
+    }
+}
+
+class BookRow extends React.Component {
+    render() {
+        const book = this.props.book;
+        const title = book.stocked ?
+            book.title :
+            <span style={{color: '#ff0000'}}>
+                {book.title}
+            </span>;
+
+        return (
+            <tr>
+                <td><img src={book.img} className="book-icons" alt="book-item"/></td>
+                <td>{title}</td>
+                <td>{book.price}</td>
+            </tr>
+        );
+    }
+}
+
+class BookTable extends React.Component {
+    render() {
+        const filterText = this.props.filterText;
+        const inStockOnly = this.props.inStockOnly;
+        const rows = [];
+        let lastCategory = null;
+
+        this.props.books.forEach((book) => {
+            if(book.title.indexOf(filterText) === -1) {
+                return;
+            }
+            if(inStockOnly && !book.stocked) {
+                return;
+            }
+            if(book.category !== lastCategory) {
+                rows.push(
+                    <BookCategoryRow
+                        category={book.category}
+                        key={book.category} />
+                );
+            }
+            rows.push(
+                <BookRow
+                    book={book}
+                    key={book.name} />
+            );
+            lastCategory = book.category;
+        });
+
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Cover</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>{rows}</tbody>
+            </table>
+        );
+    }
+}
+
+class SearchBar extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+      this.handleInStockChange = this.handleInStockChange.bind(this);
+    }
+
+    handleFilterTextChange(e) {
+      this.props.onFilterTextChange(e.target.value);
+    }
+
+    handleInStockChange(e) {
+      this.props.onInStockChange(e.target.checked);
+    }
+
+    render() {
+        return (
+            <form>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={this.props.filterText}
+                    onChange={this.handleFilterTextChange}
+                />
+                <p>
+                    <input
+                        type="checkbox"
+                        checked={this.props.inStockOnly}
+                        onChange={this.handleInStockChange}
+                     />
+                    {' '}
+                    Only show products in stock
+                </p>
+            </form>
+        );
+    }
+}
+
+class FilterableBookTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            filterText: '',
+            inStockOnly: false
+        };
+
+        this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+        this.handleInStockChange = this.handleInStockChange.bind(this);
+    }
+
+    handleFilterTextChange(filterText) {
+        this.setState({
+            filterText: filterText
+        });
+    }
+
+    handleInStockChange(inStockOnly) {
+        this.setState({
+            inStockOnly: inStockOnly
+        });
+    }
+
+    render() {
+        return (
+            <div className="bookTable">
+                <SearchBar
+                    filterText={this.state.filterText}
+                    inStockOnly={this.state.inStockOnly}
+                    onFilterTextChange={this.handleFilterTextChange}
+                    onInStockChange={this.handleInStockChange}
+                />
+                <BookTable
+                    filterText={this.state.filterText}
+                    inStockOnly={this.state.inStockOnly}
+                    books={this.props.books}
+                />
+            </div>
+        );
+    }
+}
+
+//JSON data set:
+const BOOKS = [
+    {
+        category: 'Business & Money',
+        price: '$18.95',
+        stocked: true,
+        title: 'Principles: Life and Work',
+        img: 'https://images-na.ssl-images-amazon.com/images/I/41Mq7Ss7lPL._SX331_BO1,204,203,200_.jpg'
+    },
+    {
+        category: 'Business & Money',
+        price: '$17.79',
+        stocked: true,
+        title: 'Strenghts Finder 2.0',
+        img: 'https://images-na.ssl-images-amazon.com/images/I/51SdxC2moVL._SX360_BO1,204,203,200_.jpg'
+    },
+    {
+        category: 'Business & Money',
+        price: '$14.49',
+        stocked: false,
+        title: 'The Total Money Makeover',
+        img: 'https://images.gr-assets.com/books/1328323537l/6570928.jpg'
+    },
+    {
+        category: 'Communication Skills',
+        price: '$10.14',
+        stocke: true,
+        title: 'Crucial Conversations Tools',
+        img: 'https://images-na.ssl-images-amazon.com/images/I/51W9DU9pFuL._SX330_BO1,204,203,200_.jpg'
+    },
+    {
+        category: 'Communication Skills',
+        price: '$25.26',
+        stocked: false,
+        title: 'All You Need To Know About The Music Business',
+        img: 'https://images-na.ssl-images-amazon.com/images/I/514-xeXBd9L._SX330_BO1,204,203,200_.jpg'
+    },
+    {
+        category: 'Communication Skills',
+        price: '$9.19',
+        stocked: true,
+        title: 'How to Win Friends in The Digital Age',
+        img: 'https://images-na.ssl-images-amazon.com/images/I/41WG%2B0OgDwL._SX324_BO1,204,203,200_.jpg'
+    },
+    {
+        category: 'Politics & Social Sciences',
+        price: '$17.99',
+        stocked: true,
+        title: 'Fire and Fury: Inside the Trump White House',
+        img: 'https://images-na.ssl-images-amazon.com/images/I/51AEI3isFiL._SX327_BO1,204,203,200_.jpg'
+    },
+    {
+        category: 'Politics & Social Sciences',
+        price: '$21.99',
+        stocked: false,
+        title: 'Ukraine: Zbigs Grand Chessboard',
+        img: 'https://images-na.ssl-images-amazon.com/images/I/519lt-Hc5UL._SX331_BO1,204,203,200_.jpg'
+    },
+    {
+        category: 'Politics & Social Sciences',
+        price: '$19.79',
+        stocked: true,
+        title: 'It Is Event Worse Than You Think',
+        img: 'https://images-na.ssl-images-amazon.com/images/I/419jED87sYL._SX329_BO1,204,203,200_.jpg'
+    }
+];
+
+//Testing JSX in depth:
+function BuyBtn(props) {
+    return(
+        <button backroundColor={props.backColor} color={props.fontColor} padding={props.paddingEl}>
+            Buy a ticket!
+        </button>
+    )
+}
+
+function BtnCreate() {
+    return <BuyBtn
+                backColor="red"
+                fontColor="#ffffff"
+                paddingEl="5px 10px" />;
+}
+
+//Testing props in JSX:
+function CheckingPairing(props) {
+    let description;
+    if (props.number % 2 === 0) {
+        description = <strong>even</strong>
+    } else {
+        description = <i>odd</i>
+    }
+    return <div>{props.number} is an {description} number</div>;
+}
+
+function PairingNumber() {
+    return(
+        <CheckingPairing number={43}/>
+    )
+}
+
+//Testing children with JSX:
+function Item(props) {
+    return <li>{props.message}</li>;
+}
+
+function TodoList() {
+    const todos = ['1) Complete annual report for Monarch Solutions Ltd',
+                   '2) Business meeting with Andrew Van Der Schteiger',
+                   '3) Visit to the Global Worldwide Electronical Conference',
+                   '4) Booking a new room in the hotel in Japan',
+                   '5) Meet about DFC-Defener #450567 patent',
+                   '6) Relax with my family in the weekend'
+               ];
+   return (
+       <ul>
+           {todos.map((message) => <Item key={message} message={message} />)}
+       </ul>
+   );
+}
+
+//Functions as children:
+function Repeat(props) {
+    let items = [];
+    for (let i = 0; i < props.numTimes; i++) {
+        items.push(props.children(i));
+    }
+    return <div>{items}</div>;
+}
+
+function ListOfFifteenThings() {
+    return (
+        <Repeat numTimes={15}>
+            {(index) => <div key={index}>It is a special item {index} in the list</div>}
+        </Repeat>
+    );
+}
+
+//Single child for PropTypes:
+class MyComponent extends React.Component {
+    render() {
+        const children = this.props.children;
+        return (
+            <div>
+                {children}
+            </div>
+        );
+    }
+}
+
+MyComponent.propTypes = {
+    children: PropTypes.element.isRequired
+};
+
+//Default properties initialized:
+class GreetingWindow extends React.Component {
+    render() {
+        return (
+            <div className="greeting-window">
+                <h3>Hello, {this.props.name}</h3>
+                <p>Would you like {this.props.comics} comics ?</p>
+            </div>
+        );
+    }
+}
+
+GreetingWindow.defaultProps = {
+    name: 'Guy',
+    comics: 'MARVEL'
+};
+
+//Rendering multiple componenets in the one DOM element:
+let modals = (
+    <div>
+        <Calculator />
+        <SignUpDialog />
+        <FilterableBookTable books={BOOKS} />
+        <BtnCreate />
+        <PairingNumber />
+        <TodoList />
+        <ListOfFifteenThings />
+        <GreetingWindow />
+    </div>
+)
+
 ReactDOM.render(
-  <Calculator />,
-  document.getElementById("root")
+    modals,
+    document.getElementById("root")
 );
+
 registerServiceWorker();
